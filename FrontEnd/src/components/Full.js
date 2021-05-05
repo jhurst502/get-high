c58b71722b0c119a37abbbb8bb2ce25d386fc04e
Justin Hurst <hurstjustin502@gmail.com>
2021-05-05 14:23:02 -0400

    scrolling functionality across locations

--- FrontEnd/src/components/Full.js
+++ FrontEnd/src/components/Full.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Locations from './Locations';
 
 const FullMenu = () => {
+
+    const [highlightedName, setHighlightedName] = useState(0);
+
+    const increment = () => {
+        let position = highlightedName;
+        if (position < 9) {
+            position++;
+            setHighlightedName(position);
+        }
+        else if (position === 9) {
+            setHighlightedName(0);
+        }
+        console.log(highlightedName);
+    }
+
+    const decrement = () => {
+        let position = highlightedName;
+        if (position > 0) {
+            position--;
+            setHighlightedName(position);
+        }
+        else if (position === 0) {
+            setHighlightedName(9);
+        }
+        console.log(position);
+    }
 
     return (
         <div class="z-20 absolute top-5 right-7 bg-indigo-100 rounded-lg py-4">
@@ -13,13 +39,13 @@ const FullMenu = () => {
                 </svg>
                 </button>
             </form>
-            <Locations></Locations>
+            <Locations position={highlightedName}></Locations>
             <div class="flex justify-center p-3">
-                <button class="bg-yellow-200 h-12 w-12 rounded-full shadow-md focus:outline-none hover:shadow-lg transform hover:scale-105 transition duration-200 ease-in-out">
+                <button onClick={decrement} class="bg-yellow-200 h-12 w-12 rounded-full shadow-md focus:outline-none hover:shadow-lg transform hover:scale-105 transition duration-200 ease-in-out">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M7.05 9.293L6.343 10 12 15.657l1.414-1.414L9.172 10l4.242-4.243L12 4.343z"/></svg>
                 </button>
                 <span class="px-5"></span>
-                <button class="bg-yellow-200 h-12 w-12 rounded-full shadow-md focus:outline-none hover:shadow-lg transform hover:scale-105 transition duration-200 ease-in-out">
+                <button onClick={increment} class="bg-yellow-200 h-12 w-12 rounded-full shadow-md focus:outline-none hover:shadow-lg transform hover:scale-105 transition duration-200 ease-in-out">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
                 </button>
             </div>
--- FrontEnd/src/components/Locations.js
+++ FrontEnd/src/components/Locations.js
@@ -1,23 +1,162 @@
 import React, { useState } from 'react';
 
-const Locations = () => {
+const Locations = (props) => {
 
     const [locations, setLocations] = useState(['Hill 1', 'Dog Hill', 'Mt Everest', 'Big Chungus', 'Denali', 'Mt. Elbert', 'Wetterhorn', 'Grays Peak', 'Longs Peak', 'Matterhorn'])
 
-    return (
-        <div class="text-lg text-gray-700 grid grid-rows-3 justify-start pl-7">
-            <div>{locations[0]}</div>
-            <div>{locations[1]}</div>
-            <div>{locations[2]}</div>
-            <div>{locations[3]}</div>
-            <div>{locations[4]}</div>
-            <div>{locations[5]}</div>
-            <div>{locations[6]}</div>
-            <div>{locations[7]}</div>
-            <div>{locations[8]}</div>
-            <div>{locations[9]}</div>
-        </div>
-    )
+    switch(props.position) {
+        case 0:
+            return (
+                <div class="text-lg text-gray-700 grid grid-rows-3 justify-start pl-7">
+                    <div class="py-1 ring-4 w-56 rounded-md font-medium">{locations[0]}</div>
+                    <div class="py-1">{locations[1]}</div>
+                    <div class="py-1">{locations[2]}</div>
+                    <div class="py-1">{locations[3]}</div>
+                    <div class="py-1">{locations[4]}</div>
+                    <div class="py-1">{locations[5]}</div>
+                    <div class="py-1">{locations[6]}</div>
+                    <div class="py-1">{locations[7]}</div>
+                    <div class="py-1">{locations[8]}</div>
+                    <div class="py-1">{locations[9]}</div>
+                </div>
+            );
+        case 1:
+            return (
+                <div class="text-lg text-gray-700 grid grid-rows-3 justify-start pl-7">
+                    <div class="py-1">{locations[0]}</div>
+                    <div class="py-1 ring-4 w-56 rounded-md font-medium">{locations[1]}</div>
+                    <div class="py-1">{locations[2]}</div>
+                    <div class="py-1">{locations[3]}</div>
+                    <div class="py-1">{locations[4]}</div>
+                    <div class="py-1">{locations[5]}</div>
+                    <div class="py-1">{locations[6]}</div>
+                    <div class="py-1">{locations[7]}</div>
+                    <div class="py-1">{locations[8]}</div>
+                    <div class="py-1">{locations[9]}</div>
+                </div>
+            );
+        case 2:
+            return (
+                <div class="text-lg text-gray-700 grid grid-rows-3 justify-start pl-7">
+                    <div class="py-1">{locations[0]}</div>
+                    <div class="py-1">{locations[1]}</div>
+                    <div class="py-1 ring-4 w-56 rounded-md font-medium">{locations[2]}</div>
+                    <div class="py-1">{locations[3]}</div>
+                    <div class="py-1">{locations[4]}</div>
+                    <div class="py-1">{locations[5]}</div>
+                    <div class="py-1">{locations[6]}</div>
+                    <div class="py-1">{locations[7]}</div>
+                    <div class="py-1">{locations[8]}</div>
+                    <div class="py-1">{locations[9]}</div>
+                </div>
+            );
+        case 3:
+            return (
+                <div class="text-lg text-gray-700 grid grid-rows-3 justify-start pl-7">
+                    <div class="py-1">{locations[0]}</div>
+                    <div class="py-1">{locations[1]}</div>
+                    <div class="py-1">{locations[2]}</div>
+                    <div class="py-1 ring-4 w-56 rounded-md font-medium">{locations[3]}</div>
+                    <div class="py-1">{locations[4]}</div>
+                    <div class="py-1">{locations[5]}</div>
+                    <div class="py-1">{locations[6]}</div>
+                    <div class="py-1">{locations[7]}</div>
+                    <div class="py-1">{locations[8]}</div>
+                    <div class="py-1">{locations[9]}</div>
+                </div>
+            );
+        case 4:
+            return (
+                <div class="text-lg text-gray-700 grid grid-rows-3 justify-start pl-7">
+                    <div class="py-1">{locations[0]}</div>
+                    <div class="py-1">{locations[1]}</div>
+                    <div class="py-1">{locations[2]}</div>
+                    <div class="py-1">{locations[3]}</div>
+                    <div class="py-1 ring-4 w-56 rounded-md font-medium">{locations[4]}</div>
+                    <div class="py-1">{locations[5]}</div>
+                    <div class="py-1">{locations[6]}</div>
+                    <div class="py-1">{locations[7]}</div>
+                    <div class="py-1">{locations[8]}</div>
+                    <div class="py-1">{locations[9]}</div>
+                </div>
+            );
+        case 5:
+            return (
+                <div class="text-lg text-gray-700 grid grid-rows-3 justify-start pl-7">
+                    <div class="py-1">{locations[0]}</div>
+                    <div class="py-1">{locations[1]}</div>
+                    <div class="py-1">{locations[2]}</div>
+                    <div class="py-1">{locations[3]}</div>
+                    <div class="py-1">{locations[4]}</div>
+                    <div class="py-1 ring-4 w-56 rounded-md font-medium">{locations[5]}</div>
+                    <div class="py-1">{locations[6]}</div>
+                    <div class="py-1">{locations[7]}</div>
+                    <div class="py-1">{locations[8]}</div>
+                    <div class="py-1">{locations[9]}</div>
+                </div>
+            );
+        case 6:
+            return (
+                <div class="text-lg text-gray-700 grid grid-rows-3 justify-start pl-7">
+                    <div class="py-1">{locations[0]}</div>
+                    <div class="py-1">{locations[1]}</div>
+                    <div class="py-1">{locations[2]}</div>
+                    <div class="py-1">{locations[3]}</div>
+                    <div class="py-1">{locations[4]}</div>
+                    <div class="py-1">{locations[5]}</div>
+                    <div class="py-1 ring-4 w-56 rounded-md font-medium">{locations[6]}</div>
+                    <div class="py-1">{locations[7]}</div>
+                    <div class="py-1">{locations[8]}</div>
+                    <div class="py-1">{locations[9]}</div>
+                </div>
+            );
+        case 7:
+            return (
+                <div class="text-lg text-gray-700 grid grid-rows-3 justify-start pl-7">
+                    <div class="py-1">{locations[0]}</div>
+                    <div class="py-1">{locations[1]}</div>
+                    <div class="py-1">{locations[2]}</div>
+                    <div class="py-1">{locations[3]}</div>
+                    <div class="py-1">{locations[4]}</div>
+                    <div class="py-1">{locations[5]}</div>
+                    <div class="py-1">{locations[6]}</div>
+                    <div class="py-1 ring-4 w-56 rounded-md font-medium">{locations[7]}</div>
+                    <div class="py-1">{locations[8]}</div>
+                    <div class="py-1">{locations[9]}</div>
+                </div>
+            );
+        case 8:
+            return (
+                <div class="text-lg text-gray-700 grid grid-rows-3 justify-start pl-7">
+                    <div class="py-1">{locations[0]}</div>
+                    <div class="py-1">{locations[1]}</div>
+                    <div class="py-1">{locations[2]}</div>
+                    <div class="py-1">{locations[3]}</div>
+                    <div class="py-1">{locations[4]}</div>
+                    <div class="py-1">{locations[5]}</div>
+                    <div class="py-1">{locations[6]}</div>
+                    <div class="py-1">{locations[7]}</div>
+                    <div class="py-1 ring-4 w-56 rounded-md font-medium">{locations[8]}</div>
+                    <div class="py-1">{locations[9]}</div>
+                </div>
+            );
+        case 9:
+            return (
+                <div class="text-lg text-gray-700 grid grid-rows-3 justify-start pl-7">
+                    <div class="py-1">{locations[0]}</div>
+                    <div class="py-1">{locations[1]}</div>
+                    <div class="py-1">{locations[2]}</div>
+                    <div class="py-1">{locations[3]}</div>
+                    <div class="py-1">{locations[4]}</div>
+                    <div class="py-1">{locations[5]}</div>
+                    <div class="py-1">{locations[6]}</div>
+                    <div class="py-1">{locations[7]}</div>
+                    <div class="py-1">{locations[8]}</div>
+                    <div class="py-1 ring-4 w-56 rounded-md font-medium">{locations[9]}</div>
+                </div>
+            );
+    }
+   
 }
 
 export default Locations;
