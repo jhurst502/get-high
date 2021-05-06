e23dd7ee075fb9865a8cc2b6661216de74325b34
Justin Hurst <hurstjustin502@gmail.com>
2021-05-06 11:50:21 -0400

    set up zipcode input and validation

--- FrontEnd/src/components/Full.js
+++ FrontEnd/src/components/Full.js
@@ -4,6 +4,7 @@ import Locations from './Locations';
 const FullMenu = () => {
 
     const [highlightedName, setHighlightedName] = useState(0);
+    const [zipcode, setZipcode] = useState();
 
     const increment = () => {
         let position = highlightedName;
@@ -14,7 +15,6 @@ const FullMenu = () => {
         else if (position === 9) {
             setHighlightedName(0);
         }
-        console.log(highlightedName);
     }
 
     const decrement = () => {
@@ -26,13 +26,29 @@ const FullMenu = () => {
         else if (position === 0) {
             setHighlightedName(9);
         }
-        console.log(position);
+    }
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+
+        if(zipcode.length === 5) {
+            alert(`Zipcode is ${zipcode}`);
+        }
+    }
+
+    
+    const handleChange = (e) => {
+        let regex = /^[0-9\b]+$/;
+
+        if (e.target.value ==='' || regex.test(e.target.value)) {
+            setZipcode(e.target.value)
+        }
     }
 
     return (
         <div class="z-20 absolute top-5 right-7 bg-indigo-100 rounded-lg py-4">
-            <form class="m-4 flex">
-                <input class="focus:outline-none rounded-l-lg p-2 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white" placeholder="Zipcode" />
+            <form onSubmit={handleSubmit} class="m-4 flex">
+                <input type="text" value={zipcode} onChange={handleChange} maxLength="5" class="focus:outline-none rounded-l-lg p-2 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white" placeholder="Zipcode" />
                 <button class="px-4 rounded-r-lg bg-white  text-gray-800  border-gray-200 border-t border-b border-r focus:outline-none">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 transform hover:scale-110 transition duration-200 ease-in-out" viewBox="0 0 20 20" fill="currentColor">
                     <path fillRule="evenodd" d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" clipRule="evenodd" />
