51bc17d52f620af4943c28817d5987d9c07e7253
Justin Hurst <hurstjustin502@gmail.com>
2021-05-06 17:57:53 -0400

    setup for flask restful. Retrieved zipcode data and returns coords by querying third party api

--- BackEnd/elevation.py
+++ BackEnd/elevation.py
@@ -1,6 +1,27 @@
-from flask import Flask
-app = Flask(__name__)
+import requests
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
 
-@app.route('/')
-def testing():
-    return 'No more contacting please, we are testing'
+app = Flask(__name__)
+api = Api(app)
+
+class Elevation(Resource):
+    def get(self, zipcode):
+        # Get radius of coordinates from zipcode using 
+        # us-zip-code-lattitude-and-longitude api from opendatasoft
+        coordinates = requests.get(f"https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q={zipcode}")
+        coordinates = coordinates.json()
+
+        latitude = coordinates["records"][0]["fields"]["latitude"]
+        longitude = coordinates["records"][0]["fields"]["longitude"]
+        response = {"latitude":latitude, "longitude":longitude}
+        
+        # Pass radius into Google elevation api 
+        # TODO look into marshmallow
+
+        return response
+
+api.add_resource(Elevation, '/elevation/<zipcode>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
