d8b5265a1dfc15b524a661fd17bf113d91ab8e5e
Justin Hurst <hurstjustin502@gmail.com>
2021-05-12 09:49:51 -0400

    refactor componenets

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,71 +1,7 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
 import './index.css';
+import App from './App';
 
-// eslint-disable-next-line import/no-webpack-loader-syntax
-import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
-import Menu from './components/Menu';
-
-mapboxgl.workerClass = MapboxWorker;
-mapboxgl.accessToken = 'pk.eyJ1Ijoiamh1cnN0NSIsImEiOiJja28zb2Y4cGMweDVsMnVqbmQ2MjJjYWM5In0.fNJOa1thSU7wI9IKSJSTLA';
-
-const Map = () => {
-  const mapContainer = useRef();
-  const [lng, setLng] = useState(70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
-
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox-map-design/ckhqrf2tz0dt119ny6azh975y',
-      center: [lng, lat],
-      pitch: 85,
-      bearing: 80,
-      zoom: zoom
-    });
-
-    map.on('move', () => {
-      setLng(map.getCenter().lng.toFixed(4));
-      setLat(map.getCenter().lat.toFixed(4));
-      setZoom(map.getZoom().toFixed(2));
-    });
-
-    map.on('load', function () {
-      map.addSource('mapbox-dem', {
-        'type': 'raster-dem',
-        'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
-        'tileSize': 512,
-        'maxzoom': 14
-        });
-        // add the DEM source as a terrain layer with exaggerated height
-        map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
-
-      // add a sky layer that will show when the map is highly pitched
-      map.addLayer({
-        'id': 'sky',
-        'type': 'sky',
-        'paint': {
-          'sky-type': 'atmosphere',
-          'sky-atmosphere-sun': [0.0, 0.0],
-          'sky-atmosphere-sun-intensity': 15
-        }
-      });
-    });
-    return () => map.remove();
-  }, []);
-
-  return (
-    <div>
-      <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div>
-      <Menu></Menu>
-      <div className="map-container" ref={mapContainer} />
-    </div>
-  );
-};
-
-ReactDOM.render(<Map />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
