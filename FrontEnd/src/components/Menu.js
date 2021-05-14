f14b64469a3c4ab577e45c136f32f81f81626040
Justin Hurst <hurstjustin502@gmail.com>
2021-05-14 11:22:20 -0400

    refactored default locations

--- FrontEnd/src/components/Menu.js
+++ FrontEnd/src/components/Menu.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import FullMenu from './Full';
+import startingLocations from '../defaultLocations'
 import toggle from '../animations/toggleLottie.json'
 import loadingAnimation from '../animations/loadingLottie.json';
 import lottie from 'lottie-web';
@@ -11,61 +12,6 @@ const Menu = () => {
 
     const [opened, setOpened] = useState(true);
     const [loading, setLoading] = useState(false);
-
-    // TODO put this in a separate file???
-    let startingLocations = [
-        {
-            "lng": -118.2923,
-            "lat": 36.5785,
-            "elevation": 14505
-        },
-        {
-            "lng": -119.5332,
-            "lat": 37.7459,
-            "elevation": 8839
-        },
-        {
-            "lng": -151.0070,
-            "lat": 63.0692,
-            "elevation": 20310
-        },
-        {
-            "lng": -118.2923,
-            "lat": 36.5785,
-            "elevation": 14505
-        },
-        {
-            "lng": -119.5332,
-            "lat": 37.7459,
-            "elevation": 8839
-        },
-        {
-            "lng": -151.0070,
-            "lat": 63.0692,
-            "elevation": 20310
-        },
-        {
-            "lng": -118.2923,
-            "lat": 36.5785,
-            "elevation": 14505
-        },
-        {
-            "lng": -119.5332,
-            "lat": 37.7459,
-            "elevation": 8839
-        },
-        {
-            "lng": -151.0070,
-            "lat": 63.0692,
-            "elevation": 20310
-        },
-        {
-            "lng": -119.5332,
-            "lat": 37.7459,
-            "elevation": 8839
-        }
-    ];
-
     const [locations, setLocations] = useState(startingLocations);
 
     useEffect(() => {
@@ -83,7 +29,7 @@ const Menu = () => {
             animationData: loadingAnimation
         });
         return () => animation.destroy();
-}, [loading]);
+    }, [loading]);
 
     useEffect(() => {
         if (opened) {
@@ -105,7 +51,7 @@ const Menu = () => {
     useEffect(() => {
 
     }, [loading])
-    
+
     if (loading) {
         return (
             <div class="z-20 absolute top-5 right-7 bg-indigo-100 rounded-lg py-4">
@@ -116,7 +62,7 @@ const Menu = () => {
     if (!loading) {
         return (
             <div class="z-20 absolute top-5 right-7 bg-indigo-100 rounded-lg py-4">
-                <button class="w-9 h-9 focus:outline-none p-1 absolute top-1 right-1 bg-indigo-100 rounded-lg"onClick={handle} ref={toggleContainer}/>
+                <button class="w-9 h-9 focus:outline-none p-1 absolute top-1 right-1 bg-indigo-100 rounded-lg" onClick={handle} ref={toggleContainer} />
                 {opened ? <FullMenu locations={locations} handleLocations={setLocations} handleLoading={setLoading} /> : <></>}
             </div>
         );
---
+++ FrontEnd/src/defaultLocations.js
@@ -0,0 +1,55 @@
+
+let startingLocations = [
+    {
+        "lng": -118.2923,
+        "lat": 36.5785,
+        "elevation": 14505
+    },
+    {
+        "lng": -119.5332,
+        "lat": 37.7459,
+        "elevation": 8839
+    },
+    {
+        "lng": -151.0070,
+        "lat": 63.0692,
+        "elevation": 20310
+    },
+    {
+        "lng": -118.2923,
+        "lat": 36.5785,
+        "elevation": 14505
+    },
+    {
+        "lng": -119.5332,
+        "lat": 37.7459,
+        "elevation": 8839
+    },
+    {
+        "lng": -151.0070,
+        "lat": 63.0692,
+        "elevation": 20310
+    },
+    {
+        "lng": -118.2923,
+        "lat": 36.5785,
+        "elevation": 14505
+    },
+    {
+        "lng": -119.5332,
+        "lat": 37.7459,
+        "elevation": 8839
+    },
+    {
+        "lng": -151.0070,
+        "lat": 63.0692,
+        "elevation": 20310
+    },
+    {
+        "lng": -119.5332,
+        "lat": 37.7459,
+        "elevation": 8839
+    }
+];
+
+export default startingLocations;
